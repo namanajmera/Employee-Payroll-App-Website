e0d601983a5382e086d7ab1685a95ec9359a4bec
Naman <cool.naman.ajmera@gmail.com>
2021-06-07 10:43:17 +0530

    Added: Reterive The EmployeePayroll Data From JSON Server

--- js/home_page.js
+++ js/home_page.js
@@ -1,15 +1,35 @@
 let employeePayrollDataList;
 window.addEventListener("DOMContentLoaded", (event) => {
-     employeePayrollDataList = getEmployeePayrollDataFromStorage();
-     document.querySelector(".emp-count").innerHTML = employeePayrollDataList.length;
-     createInnerHTML();
-     localStorage.removeItem('editEmp')
+     if (site_properties.use_local_storage.match("true")) {
+          getEmployeePayrollDataFromStorage();
+     } else
+          getEmployeePayrollDataFromServer();
 })
 
 const getEmployeePayrollDataFromStorage = () => {
-     return localStorage.getItem('EmployeePayrollList') ?
-          JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+     employeePayrollDataList = localStorage.getItem("EmployeePayrollList") ?
+          JSON.parse(localStorage.getItem("EmployeePayrollList")) : []
+     processEmployeePayrollDataResponse();
 }
+
+const processEmployeePayrollDataResponse = () => {
+     document.querySelector(".emp-count").innerHTML = employeePayrollDataList.length;
+     createInnerHTML();
+     localStorage.removeItem('editEmp')
+}
+
+const getEmployeePayrollDataFromServer = () => {
+     makeServiceCall("GET", site_properties.server_url, true)
+          .then(responseText => {
+               employeePayrollDataList = JSON.parse(responseText);
+               processEmployeePayrollDataResponse();
+          })
+     // .catch(error => {
+     //      console.log("GET Error Status : " + JSON.stringify(error));
+     //      employeePayrollDataList = [];
+     //      processEmployeePayrollDataResponse();
+     // });
+};
 
 const createInnerHTML = () => {
      const headerHTML = `
@@ -26,33 +46,41 @@ const createInnerHTML = () => {
      let innerHTMLData = `${headerHTML}`
      for (const employeePayrollData of employeePayrollDataList) {
           innerHTMLData = `${innerHTMLData}
-                         <tr>
-                              <td><img src="${employeePayrollData._profilePicture}" alt="" class="profile"></td>
-                              <td>${employeePayrollData._name}</td>
-                              <td>${employeePayrollData._gender}</td>
-                              <td>${getHTMLDepartment(employeePayrollData._departments)}</td>
-                              <td>₹ ${employeePayrollData._salary}</td>
-                              <td>${stringifyDate(employeePayrollData._startDate)}</td>
-                              <td>
-                                   <img id="${employeePayrollData.id}" src="/assets/design-icons/delete_black_24dp.svg" alt="delete" class="actions"
-                                        onclick="remove(this)">
-                                   <img id="${employeePayrollData.id}" src="/assets/design-icons/edit_black_24dp.svg" alt="edit" class="actions"
-                                        onclick="update(this)">
-                              </td>
-                         </tr>
+          <tr>
+               <td><img src="${employeePayrollData._profilePicture}" alt="" class="profile"></td>
+               <td>${employeePayrollData._name}</td>
+               <td>${employeePayrollData._gender}</td>
+               <td>${getDepartmentHtml(employeePayrollData._departments)}</td>
+               <td>₹ ${employeePayrollData._salary}</td>
+               <td>${stringifyDate(employeePayrollData._startDate)}</td>
+               <td>
+                    <img id="${employeePayrollData.id}" src="/assets/design-icons/delete_black_24dp.svg" alt="delete" class="actions"
+                         onclick="remove(this)">
+                    <img id="${employeePayrollData.id}" src="/assets/design-icons/edit_black_24dp.svg" alt="edit" class="actions"
+                         onclick="update(this)">
+               </td>
+          </tr>
           `;
      }
      document.querySelector("#display").innerHTML = innerHTMLData;
 }
 
-const getHTMLDepartment = (deptList) => {
-     let deptHTML = ''
-     for (const dept of deptList) {
-          deptHTML = `${deptHTML} <div class="dept-label">${dept}</div>`
+// const getHTMLDepartment = (deptList) => {
+//      let deptHTML = ''
+//      for (let dept of deptList) {
+//           deptHTML = `${deptHTML} <div class="dept-label">${dept}</div>`
+//      }
+//      return deptHTML;
+// }
+const getDepartmentHtml = (departmentList) => {
+     let departmentHtml = "";
+     if (departmentList) {
+          for (let department of departmentList) {
+               departmentHtml = `${departmentHtml} <div class="dept-label">${department}</div>`;
+          }
      }
-     return deptHTML;
-}
-
+     return departmentHtml;
+};
 const remove = (node) => {
      let employeePayrollData = employeePayrollDataList.find(empData => empData.id == node.id);
      if (!employeePayrollData) return
--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 const site_properties = {
-   use_local_storage: "true",
+   use_local_storage: "false",
    home_page: "../pages/employee_payroll_home.html",
    add_emp_payroll_page: "../pages/employee_payroll_form.html",
    server_url: "http://localhost:3000/employees_payroll/"
